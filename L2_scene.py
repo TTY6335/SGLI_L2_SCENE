9159412695a7569377a5dfa932a0bafe2967256b
Tetsuya <ogwtetsuya@gmail.com>
2020-12-27 21:23:19 +0900

    東経180西経180をまたぐときにバグるのを修正

--- L2_scene.py
+++ L2_scene.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+#ORIGINAL TTY6335
 #L2 海洋プロダクトをgeotiff出力する。
 import h5py
 import numpy as np
@@ -10,83 +11,104 @@ if __name__ == '__main__':
 
 #入力するファイルの情報#
 	#ファイル名
-        input_file=sys.argv[1]
+	input_file=sys.argv[1]
 	#バンド名
-        band_name=sys.argv[2]
+	band_name=sys.argv[2]
 
-#出力ファイル名
-        output_file=sys.argv[3]
+	#出力ファイル名
+	output_file=sys.argv[3]
 
-        try:
-            hdf_file = h5py.File(input_file, 'r')
-        except:
-            print('%s IS MISSING.' % input_file)
-            exit(1);
+	try:
+		hdf_file = h5py.File(input_file, 'r')
+	except:
+		print('%s IS MISSING.' % input_file)
+		exit(1);
 	
-        hdf_file = h5py.File(input_file, 'r')
+	hdf_file = h5py.File(input_file, 'r')
 
-        print('OPEN %s.' % input_file)
+	print('OPEN %s.' % input_file)
 
-        #L2のHDF5ファイルのImage_data以下にデータが入っている。
-        try:
-            Image_var=hdf_file['Image_data'][band_name]
-        except:
-            print('%s IS MISSING.' % band_name)
-            print('SELECT FROM')
-            print(hdf_file['Image_data'].keys())
-            exit(1);
+	#L2のHDF5ファイルのImage_data以下にデータが入っている。
+	try:
+		Image_var=hdf_file['Image_data'][band_name]
+	except:
+		print('%s IS MISSING.' % band_name)
+		print('SELECT FROM')
+		print(hdf_file['Image_data'].keys())
+		exit(1);
 
-        input_file_name=str(hdf_file['Global_attributes'].attrs['Product_file_name'][0][2:45])
+	input_file_name=str(hdf_file['Global_attributes'].attrs['Product_file_name'][0][2:45])
 
-        lat_arr=np.array(hdf_file['Geometry_data']['Latitude'],dtype='float64')
-        lon_arr=np.array(hdf_file['Geometry_data']['Longitude'],dtype='float64')
+	lat_arr=np.array(hdf_file['Geometry_data']['Latitude'],dtype='float64')
+	lon_arr=np.array(hdf_file['Geometry_data']['Longitude'],dtype='float64')
 
-        #GCPのリストを作る
-        gcp_list=[]
-        for column in range(0,lat_arr.shape[0],20):
-            for row in range(0,lat_arr.shape[1],20): 
-                gcp=gdal.GCP(lon_arr[column][row],lat_arr[column][row],0,row*10,column*10)
-                gcp_list.append(gcp)
+	#GCPのリストを作る
+	gcp_list=[]
+	#東経180度西経-180度の線を越えていないかチェック
+	top_left_lon=lon_arr[0][0]
+	top_right_lon=lon_arr[0][-1]
+	bottom_left_lon=lon_arr[-1][0]
+	bottom_right_lon=lon_arr[-1][-1]
+	
+	#東経180度西経-180度の線をまたぐ場合
+	if((top_left_lon>top_right_lon) or\
+		(top_left_lon>bottom_right_lon) or\
+		(bottom_left_lon>top_right_lon) or\
+		(bottom_left_lon>bottom_right_lon)\
+		):
+		lon_arr=np.where(lon_arr<0,lon_arr+360,lon_arr)
+		for column in range(0,lat_arr.shape[0],20):
+			for row in range(0,lat_arr.shape[1],20): 
+				gcp=gdal.GCP(lon_arr[column][row],lat_arr[column][row],0,row*10,column*10)
+				gcp_list.append(gcp)
+	#東経180度西経-180度の線をまたがない場合
+	else:	
+		for column in range(0,lat_arr.shape[0],20):
+			for row in range(0,lat_arr.shape[1],20): 
+				gcp=gdal.GCP(lon_arr[column][row],lat_arr[column][row],0,row*10,column*10)
+				gcp_list.append(gcp)
 		
-#hdfファイルに格納されているファイル名からファイルに入っていプロダクト名を選択する。
-        print(band_name)
-        band_image_arr=hdf_file['Image_data'][band_name]
-        slope=hdf_file['Image_data'][band_name].attrs['Slope']
-        offset=hdf_file['Image_data'][band_name].attrs['Offset']
 
-        #float32の行列にする
-        band_image_arr=np.array(band_image_arr,dtype='float32')
+	
+	#hdfファイルに格納されているファイル名からファイルに入っていプロダクト名を選択する。
+	print(band_name)
+	band_image_arr=hdf_file['Image_data'][band_name]
+	slope=hdf_file['Image_data'][band_name].attrs['Slope']
+	offset=hdf_file['Image_data'][band_name].attrs['Offset']
+	
+	#float32の行列にする
+	band_image_arr=np.array(band_image_arr,dtype='float32')
+	
+	#プロダクトの値を計算する
+	Error_DN=hdf_file['Image_data'][band_name].attrs['Error_DN']
+	Maximum_valid_DN=hdf_file['Image_data'][band_name].attrs['Maximum_valid_DN']
+	#陸域、エラーのピクセルはすべて-9999で埋める
+	product=np.where(band_image_arr<=Maximum_valid_DN,band_image_arr*slope+offset,-9999)
+	
+	#行数
+	row_size=product.shape[0]
+	#列数
+	col_size=product.shape[1]
 
-		#プロダクトの値を計算する
-        Error_DN=hdf_file['Image_data'][band_name].attrs['Error_DN']
-        Maximum_valid_DN=hdf_file['Image_data'][band_name].attrs['Maximum_valid_DN']
-		#陸域、エラーのピクセルはすべて-9999で埋める
-        product=np.where(band_image_arr<=Maximum_valid_DN,band_image_arr*slope+offset,-9999)
-
-        #行数
-        row_size=product.shape[0]
-        #列数
-        col_size=product.shape[1]
-
-        #出力
-        dtype = gdal.GDT_Float32
-        #バンド数
-        band=1
-        output = gdal.GetDriverByName('GTiff').Create(output_file,col_size,row_size,band,dtype)
-        output.GetRasterBand(1).WriteArray(product)
-        wkt = output.GetProjection()
-        output.SetGCPs(gcp_list,wkt)
+	#出力
+	dtype = gdal.GDT_Float32
+	#バンド数
+	band=1
+	output = gdal.GetDriverByName('GTiff').Create(output_file,col_size,row_size,band,dtype)
+	output.GetRasterBand(1).WriteArray(product)
+	wkt = output.GetProjection()
+	output.SetGCPs(gcp_list,wkt)
 	#GCPを使ってEPSG4326に投影変換
-        output = gdal.Warp(output_file,\
-			   	output,\
-				dstSRS='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',\
-				tps = True,\
-				outputType=dtype,\
-				srcNodata=Error_DN,\
-				dstNodata=-9999,\
-				multithread=True,
-				resampleAlg=gdalconst.GRIORA_NearestNeighbour)
-        output = None 	
+	output = gdal.Warp(output_file,\
+		output,\
+		dstSRS='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',\
+		tps = True,\
+		outputType=dtype,\
+		srcNodata=Error_DN,\
+		dstNodata=-9999,\
+		multithread=True,
+		resampleAlg=gdalconst.GRIORA_NearestNeighbour)
+	output = None 	
 
 
-hdf_file.close()
+	hdf_file.close()
